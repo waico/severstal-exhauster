497e38724809f135abefc8b9ff41bfd6fd1f1d58
Oleg Berezin <telepzk@gmail.com>
2023-05-29 00:29:25 +0700

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import equipments, datasets, models, frontends
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(equipments.router)
 app.include_router(datasets.router)
